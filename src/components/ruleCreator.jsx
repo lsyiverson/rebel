aab8b14f48e68b1edd24fc76c04220e74c730839
lsyiverson <lsyiverson@gmail.com>
2016-05-21 18:49:09 +0800

    [lsyiverson] - show dialog when create rule success

--- src/components/ruleCreator.jsx
+++ src/components/ruleCreator.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import {connect} from 'react-redux';
-import {Panel, Form, FormControl, Table, Button} from 'react-bootstrap';
+import {Panel, Form, FormControl, Table, Button, Modal} from 'react-bootstrap';
 
 import {clearStocks, createRule} from '../actions';
 import StockInput from './stockInput';
@@ -16,6 +16,9 @@ class RuleCreator extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.isCreateSuccess) {
       this._onCancel();
+      this.setState({
+        showDialog: true
+      })
     }
   }
 
@@ -26,13 +29,18 @@ class RuleCreator extends React.Component {
         operation: 'BUY',
         offset: '0.3',
         instant: 'false'
-      }
+      },
+      showDialog: false
     }
   }
 
   _onStockSelected(selectedStocks) {
     const stock = _.first(selectedStocks);
-    const formData = _.merge({}, this.state.formData, {stockId: stock.id, stockCode: stock.code, stockName: stock.name});
+    const formData = _.merge({}, this.state.formData, {
+      stockId: stock.id,
+      stockCode: stock.code,
+      stockName: stock.name
+    });
     this.setState({
       stock: stock,
       formData: formData
@@ -59,72 +67,87 @@ class RuleCreator extends React.Component {
     }
   }
 
+  _onCloseDialog() {
+    this.setState({
+      showDialog: false
+    })
+  }
+
   render() {
-    const {stock} = this.state;
+    const {stock, showDialog} = this.state;
     return (
-      <Panel header={<h3>创建订单</h3>}>
-        <StockInput onStockSelected={this._onStockSelected.bind(this)}/>
-
-        {
-          stock && (
-            <Form horizontal className='rule-creator__form' onSubmit={this._onSubmit.bind(this)}>
-              <Table striped bordered condensed responsive>
-                <thead>
-                <tr>
-                  <th>股票代码</th>
-                  <th>股票名称</th>
-                  <th>操作</th>
-                  <th>价格</th>
-                  <th>数量</th>
-                  <th>价格偏移</th>
-                  <th>是否立即执行</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>
-                    <FormControl {...this._getFeildProps('stockCode')} type='text' readOnly/>
-                  </td>
-                  <td>
-                    <FormControl {...this._getFeildProps('stockName')} type='text' readOnly/>
-                  </td>
-                  <td>
-                    <FormControl {...this._getFeildProps('operation')} componentClass='select'>
-                      <option value='BUY'>{operationType.BUY}</option>
-                      <option value='SELL'>{operationType.SELL}</option>
-                    </FormControl>
-                  </td>
-                  <td>
-                    <FormControl {...this._getFeildProps('price')} type='number' min='0' step='0.01'/>
-                  </td>
-                  <td>
-                    <FormControl {...this._getFeildProps('volumn')} type='number' min='100' step='100'/>
-                  </td>
-                  <td>
-                    <FormControl {...this._getFeildProps('offset')} type='number' min='0' step='0.01'/>
-                  </td>
-                  <td>
-                    <FormControl {...this._getFeildProps('instant')} componentClass='select'>
-                      <option value='true'>是</option>
-                      <option value='false'>否</option>
-                    </FormControl>
-                  </td>
-                </tr>
-                </tbody>
-              </Table>
-
-              <div className='rule-creator__buttons'>
-                <Button bsStyle='info' onClick={this._onCancel.bind(this)}>
-                  取消
-                </Button>
-                <Button bsStyle='primary' type='submit'>
-                  提交
-                </Button>
-              </div>
-            </Form>
-          )
-        }
-      </Panel>
+      <div>
+        <Modal show={showDialog}>
+          <Modal.Header>
+            <Modal.Title>创建订单成功</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>你可以在列表中查看新创建的订单</Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle='primary' onClick={this._onCloseDialog.bind(this)}>确定</Button>
+          </Modal.Footer>
+        </Modal>
+        <Panel header={<h3>创建订单</h3>}>
+          <StockInput onStockSelected={this._onStockSelected.bind(this)}/>
+          {
+            stock && (
+              <Form horizontal className='rule-creator__form' onSubmit={this._onSubmit.bind(this)}>
+                <Table striped bordered condensed responsive>
+                  <thead>
+                  <tr>
+                    <th>股票代码</th>
+                    <th>股票名称</th>
+                    <th>操作</th>
+                    <th>价格</th>
+                    <th>数量</th>
+                    <th>价格偏移</th>
+                    <th>是否立即执行</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>
+                      <FormControl {...this._getFeildProps('stockCode')} type='text' readOnly/>
+                    </td>
+                    <td>
+                      <FormControl {...this._getFeildProps('stockName')} type='text' readOnly/>
+                    </td>
+                    <td>
+                      <FormControl {...this._getFeildProps('operation')} componentClass='select'>
+                        <option value='BUY'>{operationType.BUY}</option>
+                        <option value='SELL'>{operationType.SELL}</option>
+                      </FormControl>
+                    </td>
+                    <td>
+                      <FormControl {...this._getFeildProps('price')} type='number' min='0' step='0.01'/>
+                    </td>
+                    <td>
+                      <FormControl {...this._getFeildProps('volumn')} type='number' min='100' step='100'/>
+                    </td>
+                    <td>
+                      <FormControl {...this._getFeildProps('offset')} type='number' min='0' step='0.01'/>
+                    </td>
+                    <td>
+                      <FormControl {...this._getFeildProps('instant')} componentClass='select'>
+                        <option value='true'>是</option>
+                        <option value='false'>否</option>
+                      </FormControl>
+                    </td>
+                  </tr>
+                  </tbody>
+                </Table>
+                <div className='rule-creator__buttons'>
+                  <Button bsStyle='info' onClick={this._onCancel.bind(this)}>
+                    取消
+                  </Button>
+                  <Button bsStyle='primary' type='submit'>
+                    提交
+                  </Button>
+                </div>
+              </Form>
+            )
+          }
+        </Panel>
+      </div>
     );
   }
 }
